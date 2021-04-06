93d56d6b64e9b900983a120221068a3aa3d70d75
Naser Ahadi <Ahadi.N@orchidpharmed.com>
2021-04-06 16:54:42 +0430

    1st: Transition by using javascript hooks and velocity

---
+++ src/views/Drawer.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <button @click="isOpen = !isOpen">
+            My Profile
+        </button>
+
+        <transition
+            @before-enter="beforeEnter"
+            @enter="enter"
+            @leave="leave"
+            :css="false"
+        >
+            <div v-if="isOpen"  class="drawer">
+                <div>naser</div>
+                <div>vue programmer</div>
+                <div>30 years old</div>
+            </div>
+        </transition>
+    </div>
+</template>
+
+<script>
+    import Velocity from "velocity-animate"
+    export default {
+        name:"Drawer",
+        data(){
+            return{
+                isOpen:false
+            }
+        },
+        methods:{
+            beforeEnter(el){
+                el.style.opacity = 0
+                el.style.width = "0em"
+            },
+            enter(el, done){
+                Velocity(
+                            el, 
+                            {opacity:1, width:"12em"},
+                            {duration: 1000, easing:"easeOutCubic", complete: done}
+                        )
+            },
+            leave(el, done){
+                Velocity(
+                            el, 
+                            {opacity:0, width:"0em"},
+                            {duration: 500, easing:"easeInCubic", complete: done}
+                        )
+            },
+        }
+    }
+</script>
+
+<style scoped>
+.drawer{
+    background-color: #ccc;
+    height: 100%;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    top: 0;
+}
+</style>
