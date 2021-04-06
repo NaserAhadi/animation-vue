f8d8d47e36eccd6318223f4df558a35c486d3101
Naser Ahadi <Ahadi.N@orchidpharmed.com>
2021-04-06 16:53:27 +0430

    1st : Transition group and move

---
+++ src/views/List.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="about">
+    <h1>This is an list page</h1>
+    <input type="text" v-model="newContact">
+    <button @click="addContact">Add Contact</button>
+    <button @click="sortContact">Sort</button>
+
+    <transition-group name="slide-up" tag="ul" appear>
+      <li 
+        v-for="contact in contacts"
+        :key="contact"
+      >
+        {{ contact }}
+      </li>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      newContact:"",
+      contacts:["Naser Ahadi", "Ali Ghandehari", "Vahid Rastgoo"]
+    }
+  },
+  methods:{
+    addContact(){
+      this.contacts.push(this.newContact);
+      this.newContact=""
+    },
+    sortContact(){
+      this.contacts.sort()
+    }
+  }
+}
+</script>
+
+<style scoped>
+li{
+  list-style: none;
+}
+</style>
