d397188f540c31c75950f56ee61db3952ddad5f6
Naser Ahadi <Ahadi.N@orchidpharmed.com>
2021-04-06 16:52:56 +0430

    1st: Transition

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">Modal</router-link> |
+      <router-link to="/list">List</router-link> |
+      <router-link to="/drawer">Drawer</router-link>
+    </div>
+    <transition name="slide-fade" mode="out-in">
+      <router-view/>
+    </transition>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -23,6 +18,58 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+/*** TRANSITIONS ***/
+.fade-enter {
+  opacity:0
+}
+
+.fade-enter-active,
+.fade-leave-active{
+  transition:opacity 0.5s ease-out
+}
+
+.fade-leave-to {
+  opacity: 0;
+}
+
+.slide-fade-enter {
+  opacity:0;
+  transform: translateX(50px)
+}
+
+.slide-fade-enter-active,
+.slide-fade-leave-active{
+  transition:all 0.5s ease
+}
+
+.slide-fade-leave-to {
+  opacity: 0;
+  transform: translateX(-50px)
+}
+
+.slide-up-enter {
+  transform:translateY(10px)
+}
+
+.slide-up-enter-active{
+  transition:all 0.5s ease-out
+}
+
+.slide-up-move {
+  transition:transform 0.8s ease-in;
 }
 </style>
---
+++ src/views/Modal.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="home">
+    <h1>This is Modal Page</h1>
+    <button class="button" @click="showTheBox">Open</button>
+    <transition name="fade">
+      <div class="close-container" v-if="isShow">
+        <button 
+          class="button clase-button"
+          @click="hideTheBox"
+        >
+          Close
+        </button>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Modal',
+  data(){
+    return{
+      isShow: false
+    }
+  },
+  methods:{
+    showTheBox(){
+      this.isShow = true
+    },
+    hideTheBox(){
+      this.isShow = false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.button {
+  outline: none;
+  border-radius: 0.3rem;
+  background-color: #333;
+  color: #fff;
+  border: none;
+  padding: 0.2rem 0.5rem;
+  cursor: pointer;
+}
+
+.close-container {
+  background-color: #ccc;
+  margin: 2rem auto;
+  width: 12rem;
+  height: 12rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 1rem;
+}
+
+.clase-button {
+  background-color: coral;
+}
+</style>
